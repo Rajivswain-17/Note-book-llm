caa4ac9d64753786720c521d501ace7c296c15d0
Rajiv Swain <rajivswain12367@gmail.com>
2026-08-05 17:01:04 +0530

    feat: initialize server with Better Auth, Prisma ORM, and Express, and update PostgreSQL port in docker-compose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express"
 import "dotenv/config";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express()
 const PORT = process.env.PORT;
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 
 app.get("/", (req, res) => {
